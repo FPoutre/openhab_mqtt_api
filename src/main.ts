52bf6ffb2604df9f9eb5cb1a328dff54662ead47
F/Poutre <kevin@fpout.re>
2021-05-18 13:56:57 +0200

    5

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,5 @@
-const http = require('http');
+import * as RESTHandler from './RESTHandler'; 
+
 const mqtt = require('mqtt');
 
-function getItem(itemName: string) {
-    http.get(
-        'http://localhost:8080/rest/items/' + itemName,
-        (res : any) => {
-            let data : any[] = [];
-
-            res.on('data', (chunk : any) => {
-                data.push(chunk);
-            });
-            
-            res.on('end', () => {
-                console.log('Response ended: ');
-                let json = JSON.parse(Buffer.concat(data).toString());
-                console.log(json);
-            });
-        }
-    );
-}
-
-getItem('zWaveWallPlug4_Switch');
+RESTHandler.getItem('zWaveWallPlug4_Switch');
