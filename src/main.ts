5675c3c9236f258965e852066f9f0f0ea78f49f2
F/Poutre <kevin@fpout.re>
2021-05-18 11:50:31 +0200

    3

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-const requests = require('requests');
+const http = require('http');
 const mqtt = require('mqtt');
 
 function getItem(itemName: string) {
-    requests(
-        'http://localhost:8080/rest/item/' + itemName, 
-        {json: true},
-        (err : any, res : any, body : any) => {
-            console.log(res);
+    http.get(
+        'http://localhost:8080/rest/items/' + itemName,
+        (res : any) => {
+            let data : any[] = [];
+
+            res.on('data', (chunk : any) => {
+                data.push(chunk);
+            });
+            
+            res.on('end', () => {
+                console.log('Response ended: ');
+                let json = JSON.parse(Buffer.concat(data).toString());
+                console.log(json);
+            });
         }
-    )
+    );
 }
 
 getItem('zWaveWallPlug4');
